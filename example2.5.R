6e665a9cc61845b3cd91ae9862934d91707b4888
he-zhang-hsu <82065560+he-zhang-hsu@users.noreply.github.com>
2021-04-08 20:22:05 -0400

    Add files via upload

---
+++ example2.5.R
@@ -0,0 +1,509 @@
+################## data generate ##############
+# Program: boxcox_impute_expolore_simulation; ############
+# generate data, and try out different missing data methods;
+# run some simulations to assess the biasedness of various methods;
+
+# library(car);
+# library(mvtnorm);
+library(mice);
+library(norm);
+# library(HI);
+options(digits=4);
+rm(list=ls());
+
+
+
+parameter_draw=function(data)
+{
+ # obtain all the computational components;
+ p=ncol(data);
+ n_total=nrow(data);
+ x=cbind(rep(1,n_total), data[,1:(p-1)]);
+ y=data[,p];
+ # perform least-square calculation;
+ beta_hat=solve(t(x)%*%x)%*%t(x)%*%y;
+ s_square=1/(n_total-p)*crossprod(y-x%*%beta_hat);
+ inv_v_beta=solve(t(x)%*%x);
+ 
+ # draw sigma_square;
+ # set.seed(19760421);
+ sigma_square=1/(rgamma(1, shape=n_total/2-p/2, scale=2/((n_total-p)*s_square)));
+ 
+ # draw beta;
+ beta=mvrnorm(n=1, mu=beta_hat, Sigma=sigma_square*inv_v_beta);
+
+ parameter=c(beta,sigma_square);
+ parameter;
+}
+
+
+improper_imputation=function(data)
+{
+ # obtain all the computational components;
+ p=ncol(data);
+ n_total=nrow(data);
+ x=cbind(rep(1,n_total), data[,1:(p-1)]);
+ y=data[,p];
+ # perform least-square calculation;
+ beta_hat=solve(t(x)%*%x)%*%t(x)%*%y;
+ s_square=1/(n_total-p)*crossprod(y-x%*%beta_hat);
+ inv_v_beta=solve(t(x)%*%x);
+ 
+ # draw sigma_square;
+ # set.seed(19760421);
+ sigma_square=1/(rgamma(1, shape=n_total/2-p/2, scale=2/((n_total-p)*s_square)));
+ 
+ # draw beta;
+ # beta=mvrnorm(n=1, mu=beta_hat, Sigma=sigma_square*inv_v_beta);
+
+ parameter=c(beta_hat,sigma_square);
+ parameter;
+}
+
+
+
+
+
+rowobs=1000;
+
+# set up the random seed;
+set.seed(197789);
+
+# generate data;
+# distribution of x, the covariate;
+# could be normal, or could be other distributions;
+# fixed at mean 10, and variance 1;
+mu_x=1;
+var_x=1;
+
+# regression parameters;
+# both beta0, beta1 and beta2 are fixed at 
+beta0=-2;
+# beta1=0;
+beta1=1;
+
+# error variance;
+var_error=1;
+
+
+cycle_no=1000;
+
+# matrices holding the parameter estimates;
+# complete-data inferences;
+# xyslope means regressing x on y;
+BD_mean_vec=BD_mean_var_vec=BD_slope_vec=BD_slope_var_vec=BD_xyslope_vec=BD_xyslope_var_vec=rep(NA, cycle_no);
+CC_mean_vec=CC_mean_var_vec=CC_slope_vec=CC_slope_var_vec=CC_xyslope_vec=CC_xyslope_var_vec=rep(NA, cycle_no);
+SI_mean_vec=SI_mean_var_vec=SI_slope_vec=SI_slope_var_vec=SI_xyslope_vec=SI_xyslope_var_vec=rep(NA, cycle_no);
+INDI_xyslope_vec=INDI_xyslope_var_vec=rep(NA, cycle_no);
+
+
+
+for (cycle in 1:cycle_no)
+
+{
+set.seed(cycle);
+
+x=mu_x+sqrt(var_x)*rnorm(rowobs);
+
+# error distribution
+# could be normal, or could be other distributions;
+error=sqrt(var_error)*rnorm(rowobs);
+
+# generate the latent variable;
+y=beta0+beta1*x+error;
+
+# set up the missing data as MCAR on x;
+# miss_indi=runif(n=rowobs)<0.40;
+
+# set up the missing data as MAR on x;
+alpha0=-1.4;
+alpha1=1;
+miss_indi=rbinom(n=rowobs, size=1, prob=exp(alpha0+alpha1*x)/(1+exp(alpha0+alpha1*x)));
+
+
+obs_indi=1-miss_indi;
+
+y_miss=y;
+y_miss[miss_indi==1]=NA;
+miss_seq=cbind(seq(1,rowobs,1), miss_indi)[miss_indi==1,1];
+obs_no=length(y_miss[!is.na(y_miss)]);
+mis_no=rowobs-obs_no;
+y_obs=y[miss_indi==0];
+x_obs=x[miss_indi==0];
+x_mis=x[miss_indi==1];
+
+# compelete data analysis;
+# marginal mean;
+BD_mean_vec[cycle]=mean(y);
+BD_mean_var_vec[cycle]=var(y)/rowobs;
+
+
+# regression analysis;
+BD=summary(lm(y~x));
+
+BD_slope_vec[cycle]=BD$coeff[2,1];
+BD_slope_var_vec[cycle]=BD$coeff[2,2]^2;
+
+# reverse regression analysis;
+BD_xy=summary(lm(x~y));
+
+BD_xyslope_vec[cycle]=BD_xy$coeff[2,1];
+BD_xyslope_var_vec[cycle]=BD_xy$coeff[2,2]^2;
+
+
+# different missing data methods;
+
+# complete-case analysis;
+
+# marginal mean;
+CC_mean_vec[cycle]=mean(y_obs);
+CC_mean_var_vec[cycle]=var(y_obs)/obs_no;
+
+
+# regression analysis;
+CC=summary(lm(y_obs~x_obs));
+
+CC_slope_vec[cycle]=CC$coeff[2,1];
+CC_slope_var_vec[cycle]=CC$coeff[2,2]^2;
+
+# reverse regression analysis;
+CC_xy=summary(lm(x_obs~y_obs));
+
+CC_xyslope_vec[cycle]=CC_xy$coeff[2,1];
+CC_xyslope_var_vec[cycle]=CC_xy$coeff[2,2]^2;
+
+# missing data indicator method;
+y_miss_0=y_miss;
+y_miss_0[miss_indi==1]=0;
+
+INDI_xy=summary(lm(x ~ obs_indi+y_miss_0));
+INDI_xyslope_vec[cycle]=INDI_xy$coeff[3,1];
+INDI_xyslope_var_vec[cycle]=INDI_xy$coeff[3,2]^2;
+
+
+
+# now impute the missing y2's to get estimates of the marginal;
+y_completed=y_miss;
+
+# single imputation method;
+y_imputed=mice.impute.norm.predict(y_miss, ry=as.logical(1-miss_indi), seed=i, x=x);
+
+
+y_completed[miss_seq]=y_imputed;
+
+# completed-data analysis;
+
+# marginal mean;
+SI_mean_vec[cycle]=mean(y_completed);
+SI_mean_var_vec[cycle]=var(y_completed)/rowobs;
+
+# regression analysis;
+
+IMP=summary(lm(y_completed~x));
+
+SI_slope_vec[cycle]=IMP$coeff[2,1];
+SI_slope_var_vec[cycle]=IMP$coeff[2,2]^2;
+
+# reverse regression analysis;
+
+IMP_xy=summary(lm(x~y_completed));
+
+SI_xyslope_vec[cycle]=IMP_xy$coeff[2,1];
+SI_xyslope_var_vec[cycle]=IMP_xy$coeff[2,2]^2;
+
+
+cat("the cycle is", cycle, "\n");
+
+
+}
+
+# the end of multiple imputation;
+
+# plot the distribution of the data;
+
+# check the performance of the estimates;
+# population quantity
+
+#############################################################################
+# marginal mean estimand;
+true_mean=mean(BD_mean_vec);
+true_mean;
+
+# performance
+BD_mean_mse=mean((BD_mean_vec-true_mean)^2);
+BD_mean_mse;
+
+# lower and upper 95% CI;
+BD_mean_low95_vec=BD_mean_vec-1.96*sqrt(BD_mean_var_vec);
+BD_mean_up95_vec=BD_mean_vec+1.96*sqrt(BD_mean_var_vec);
+
+BD_mean_length=mean(BD_mean_up95_vec-BD_mean_low95_vec);
+BD_mean_length;
+
+BD_mean_cov=(BD_mean_low95_vec < true_mean)*(BD_mean_up95_vec > true_mean);
+
+mean(BD_mean_cov);
+
+# variance estimates;
+mean(sqrt(BD_mean_var_vec));
+sqrt(var(BD_mean_vec));
+
+
+# complete case analysis;
+
+CC_mean_bias=mean(CC_mean_vec)-true_mean;
+CC_mean_bias;
+
+CC_mean_mse=mean((CC_mean_vec-true_mean)^2);
+CC_mean_mse;
+
+# lower and upper 95% CI;
+CC_mean_low95_vec=CC_mean_vec-1.96*sqrt(CC_mean_var_vec);
+CC_mean_up95_vec=CC_mean_vec+1.96*sqrt(CC_mean_var_vec);
+
+CC_mean_length=mean(CC_mean_up95_vec-CC_mean_low95_vec);
+CC_mean_length;
+
+CC_mean_cov=(CC_mean_low95_vec < true_mean)*(CC_mean_up95_vec > true_mean);
+
+mean(CC_mean_cov);
+
+# variance estimates;
+mean(sqrt(CC_mean_var_vec));
+sqrt(var(CC_mean_vec));
+
+
+# single imputation estimates;
+
+SI_mean_bias=mean(SI_mean_vec)-true_mean;
+SI_mean_bias;
+
+SI_mean_mse=mean((SI_mean_vec-true_mean)^2);
+SI_mean_mse;
+
+# lower and upper 95% CI;
+SI_mean_low95_vec=SI_mean_vec-1.96*sqrt(SI_mean_var_vec);
+SI_mean_up95_vec=SI_mean_vec+1.96*sqrt(SI_mean_var_vec);
+
+SI_mean_length=mean(SI_mean_up95_vec-SI_mean_low95_vec);
+SI_mean_length;
+
+SI_mean_cov=(SI_mean_low95_vec < true_mean)*(SI_mean_up95_vec > true_mean);
+
+mean(SI_mean_cov);
+
+# variance estimates;
+mean(sqrt(SI_mean_var_vec));
+sqrt(var(SI_mean_vec));
+
+
+
+
+##########################################################################
+# slope estimand;
+
+# complete-data analysis;
+
+true_slope=mean(BD_slope_vec);
+true_slope;
+
+
+# performance;
+BD_slope_mse=mean((BD_slope_vec-true_slope)^2);
+BD_slope_mse;
+
+# lower and upper 95% CI;
+BD_slope_low95_vec=BD_slope_vec-1.96*sqrt(BD_slope_var_vec);
+BD_slope_up95_vec=BD_slope_vec+1.96*sqrt(BD_slope_var_vec);
+
+BD_slope_length=mean(BD_slope_up95_vec-BD_slope_low95_vec);
+BD_slope_length;
+
+BD_slope_cov=(BD_slope_low95_vec < true_slope)*(BD_slope_up95_vec > true_slope);
+mean(BD_slope_cov);
+
+# variance estimates;
+mean(sqrt(BD_slope_var_vec));
+sqrt(var(BD_slope_vec));
+
+
+# complete-case analysis;
+CC_slope_bias=mean(CC_slope_vec)-true_slope;
+CC_slope_bias;
+
+CC_slope_mse=mean((CC_slope_vec-true_slope)^2);
+CC_slope_mse;
+
+# lower and upper 95% CI;
+CC_slope_low95_vec=CC_slope_vec-1.96*sqrt(CC_slope_var_vec);
+CC_slope_up95_vec=CC_slope_vec+1.96*sqrt(CC_slope_var_vec);
+
+CC_slope_length=mean(CC_slope_up95_vec-CC_slope_low95_vec);
+CC_slope_length;
+
+CC_slope_cov=(CC_slope_low95_vec < true_slope)*(CC_slope_up95_vec > true_slope);
+
+mean(CC_slope_cov);
+
+# variance estimates;
+mean(sqrt(CC_slope_var_vec));
+sqrt(var(CC_slope_vec));
+
+
+# Single imputation estimates;
+
+SI_slope_bias=mean(SI_slope_vec)-true_slope;
+SI_slope_bias;
+
+SI_slope_mse=mean((SI_slope_vec-true_slope)^2);
+SI_slope_mse;
+
+# lower and upper 95% CI;
+SI_slope_low95_vec=SI_slope_vec-1.96*sqrt(SI_slope_var_vec);
+SI_slope_up95_vec=SI_slope_vec+1.96*sqrt(SI_slope_var_vec);
+
+SI_slope_length=mean(SI_slope_up95_vec-SI_slope_low95_vec);
+SI_slope_length;
+
+SI_slope_cov=(SI_slope_low95_vec < true_slope)*(SI_slope_up95_vec > true_slope);
+
+mean(SI_slope_cov);
+
+# variance estimates;
+mean(sqrt(SI_slope_var_vec));
+sqrt(var(SI_slope_vec));
+
+
+#######################################################################
+# reverse slope;
+
+# complete-data analysis;
+
+true_xyslope=mean(BD_xyslope_vec);
+true_xyslope;
+
+
+# performance;
+BD_xyslope_mse=mean((BD_xyslope_vec-true_xyslope)^2);
+BD_xyslope_mse;
+
+# lower and upper 95% CI;
+BD_xyslope_low95_vec=BD_xyslope_vec-1.96*sqrt(BD_xyslope_var_vec);
+BD_xyslope_up95_vec=BD_xyslope_vec+1.96*sqrt(BD_xyslope_var_vec);
+
+BD_xyslope_length=mean(BD_xyslope_up95_vec-BD_xyslope_low95_vec);
+BD_xyslope_length;
+
+BD_xyslope_cov=(BD_xyslope_low95_vec < true_xyslope)*(BD_xyslope_up95_vec > true_xyslope);
+mean(BD_xyslope_cov);
+
+# variance estimates;
+mean(sqrt(BD_xyslope_var_vec));
+sqrt(var(BD_xyslope_vec));
+
+
+# complete-case analysis;
+CC_xyslope_bias=mean(CC_xyslope_vec)-true_xyslope;
+CC_xyslope_bias;
+
+CC_xyslope_mse=mean((CC_xyslope_vec-true_xyslope)^2);
+CC_xyslope_mse;
+
+# lower and upper 95% CI;
+CC_xyslope_low95_vec=CC_xyslope_vec-1.96*sqrt(CC_xyslope_var_vec);
+CC_xyslope_up95_vec=CC_xyslope_vec+1.96*sqrt(CC_xyslope_var_vec);
+
+CC_xyslope_length=mean(CC_xyslope_up95_vec-CC_xyslope_low95_vec);
+CC_xyslope_length;
+
+CC_xyslope_cov=(CC_xyslope_low95_vec < true_xyslope)*(CC_xyslope_up95_vec > true_xyslope);
+
+mean(CC_xyslope_cov);
+
+# variance estimates;
+mean(sqrt(CC_xyslope_var_vec));
+sqrt(var(CC_xyslope_vec));
+
+# missing data indicator method;
+
+INDI_xyslope_bias=mean(INDI_xyslope_vec)-true_xyslope;
+INDI_xyslope_bias;
+
+INDI_xyslope_mse=mean((INDI_xyslope_vec-true_xyslope)^2);
+INDI_xyslope_mse;
+
+# lower and upper 95% CI;
+INDI_xyslope_low95_vec=INDI_xyslope_vec-1.96*sqrt(INDI_xyslope_var_vec);
+INDI_xyslope_up95_vec=INDI_xyslope_vec+1.96*sqrt(INDI_xyslope_var_vec);
+
+INDI_xyslope_length=mean(INDI_xyslope_up95_vec-INDI_xyslope_low95_vec);
+INDI_xyslope_length;
+
+INDI_xyslope_cov=(INDI_xyslope_low95_vec < true_xyslope)*(INDI_xyslope_up95_vec > true_xyslope);
+
+mean(INDI_xyslope_cov);
+
+# variance estimates;
+mean(sqrt(INDI_xyslope_var_vec));
+sqrt(var(INDI_xyslope_vec));
+
+
+# single imputation estimates;
+
+SI_xyslope_bias=mean(SI_xyslope_vec)-true_xyslope;
+SI_xyslope_bias;
+
+SI_xyslope_mse=mean((SI_xyslope_vec-true_xyslope)^2);
+SI_xyslope_mse;
+
+# lower and upper 95% CI;
+SI_xyslope_low95_vec=SI_xyslope_vec-1.96*sqrt(SI_xyslope_var_vec);
+SI_xyslope_up95_vec=SI_xyslope_vec+1.96*sqrt(SI_xyslope_var_vec);
+
+SI_xyslope_length=mean(SI_xyslope_up95_vec-SI_xyslope_low95_vec);
+SI_xyslope_length;
+
+SI_xyslope_cov=(SI_xyslope_low95_vec < true_xyslope)*(SI_xyslope_up95_vec > true_xyslope);
+
+mean(SI_xyslope_cov);
+
+# variance estimates;
+mean(sqrt(SI_xyslope_var_vec));
+sqrt(var(SI_xyslope_vec));
+
+# plot;
+
+y_plot_obs=y;
+y_plot_miss=y;
+y_plot_obs[miss_indi==1]=NA;
+y_plot_miss[miss_indi==0]=NA;
+cbind(y_plot_obs, y_plot_miss);
+
+plot(x,y);
+
+y_plot=cbind(y_plot_obs, y_plot_miss);
+
+# plot the observed and missing points;
+# plot of example 2.5;
+matplot(x[1:250], y_plot[1:250,], pch=c(1,16), main="Scatter plot before deletion",
+ xlab="x", ylab="y");
+# matplot(y_plot[1:250,], x[1:250], pch=c(1,16), xlab="y", ylab="x");
+
+# plot of example 2.5;
+hist(y_plot_miss, xlab="y_miss", main="Histogram of deleted values");
+
+# plot the observed and imputed points;
+
+y_plot_imputed=y_completed;
+y_plot_imputed[miss_indi==0]=NA;
+y_plot_impute=cbind(y_plot_obs, y_plot_imputed);
+
+# plot of example 2.5;
+hist(y_plot_imputed, xlab="y_predicted", main="Histogram of predicted values");
+
+# plot of example 2.5;
+matplot(x[1:250], y_plot_impute[1:250,], pch=c(1,17), xlab="x", ylab="y",
+   main="Scatter plot after regression prediction");
+
+# matplot(y_plot_impute[1:250,], x[1:250], pch=c(1,17), xlab="y", ylab="x");
+
